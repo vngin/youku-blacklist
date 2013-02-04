08fe3c41b76eed24b29110395db1fde82d4e4c2e
ibsen <v@vngin.com>
2013-02-04 21:26:25 +0800

    delete some comments

--- content.js
+++ content.js
@@ -1,22 +1,6 @@
 /*******************************
  * load this script in youku.com 
  *******************************/
-// make a video object from youku's DOM
-// <ul class="v">
-    // <li class="v_link"><a onmouseover="if(typeof(VideoPop) != 'undefined') VideoPop.showVTip(event)" onmouseout="if(typeof(VideoPop) != 'undefined') VideoPop.hideVTip(event)" href="http://v.youku.com/v_show/id_XNTA1MTI1ODY4.html?f=18886807" charset="106302-51598-2-1" target="video"></a></li>
-    // <li class="v_thumb">
-    //     <img src="http://g2.ykimg.com/0100401F4650FE07F9A26B0000000196B3538A-3691-0BBD-8BB5-0D6832761177?u=1358825466" alt="奥巴马就职演说全程" title="奥巴马就职演说全程">
-    // </li>
-    // <li class="v_ishd"><span class="ico__SD" title="超清"></span> </li>
-    // <li class="v_menu" id="PlayListFlag_XNTA1MTI1ODY4" style="display: none;"><span class="ico__listadd" title="添加到点播单"></span></li>
-    // <li class="v_subhead"><span class="subtitle">强调人人平等自由</span><span class="bg"></span></li>
-    // <li class="v_title"><a href="http://v.youku.com/v_show/id_XNTA1MTI1ODY4.html?f=18886807" charset="106302-51598-2-2" target="video" title="奥巴马就职演说全程">奥巴马就职演说全程</a></li>
-    // <li class="v_stat"><span class="ico__statplay" title="播放"></span><span class="num">134,299</span> <span title="评论" class="ico__statcomment"></span><span class="num">977</span></li>
-// </ul>
-// OR
-// <ul class="p">
-//   <li class="p_thumb">
-// ...
 
 function make_video(anchor) {
 
